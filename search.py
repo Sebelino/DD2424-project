a709787d35ea7ee5b80defd26eeca975bb596251
Ville Sebastian Olsson <seo@kth.se>
2025-05-13 14:53:52 +0200

    feat: Support specifying None with fallback

--- search.py
+++ search.py
@@ -19,14 +19,20 @@ else:
     memory = Memory(location=None, verbose=0)
 
 
+def fallback(a, trial, b):
+    if a is None:
+        return b
+    return a(trial)
+
+
 @dataclass
 class SearchParams:
-    learning_rate: Callable[[Trial], float]
-    weight_decay: Callable[[Trial], float]
-    momentum: Callable[[Trial], float]
-    momentum: Callable[[Trial], float]
-    unfreeze_epoch_1: Callable[[Trial], int]
-    unfreeze_epoch_2: Callable[[Trial, int], int]
+    learning_rate: Callable[[Trial], float] = None
+    weight_decay: Callable[[Trial], float] = None
+    momentum: Callable[[Trial], float] = None
+    augmentation: Callable[[Trial], bool] = None
+    unfreeze_epoch_1: Callable[[Trial], int] = None
+    unfreeze_epoch_2: Callable[[Trial, int], int] = None
 
 
 def objective(dataset_params: DatasetParams, params: TrainParams, search_params: SearchParams, determinism: Determinism,
@@ -34,14 +40,15 @@ def objective(dataset_params: DatasetParams, params: TrainParams, search_params:
     dataset_params = dataset_params.copy()
     params = params.copy()
 
-    params.optimizer.learning_rate = search_params.learning_rate(trial)
-    params.optimizer.weight_decay = search_params.weight_decay(trial)
-    params.optimizer.momentum = search_params.momentum(trial)
+    params.optimizer.learning_rate = fallback(search_params.learning_rate, trial, params.optimizer.learning_rate)
+    params.optimizer.weight_decay = fallback(search_params.weight_decay, trial, params.optimizer.weight_decay)
+    params.optimizer.momentum = fallback(search_params.momentum, trial, params.optimizer.momentum)
+    params.augmentation.enabled = fallback(search_params.augmentation, trial, params.augmentation.enabled)
 
-    unfreeze_1 = search_params.unfreeze_epoch_1(trial)
+    unfreeze_1 = search_params.unfreeze_epoch_1(trial) if search_params.unfreeze_epoch_1 is not None else None
     # Ensure second unfreeze is after first
-    unfreeze_2 = search_params.unfreeze_epoch_2(trial, unfreeze_1)
-    params.unfreezing_epochs = (unfreeze_1, unfreeze_2)
+    unfreeze_2 = search_params.unfreeze_epoch_2(trial, unfreeze_1) if search_params.unfreeze_epoch_2 is not None else None
+    params.unfreezing_epochs = (unfreeze_1, unfreeze_2) if unfreeze_1 is not None and unfreeze_2 is not None else params.unfreezing_epochs
 
     return objective_run(dataset_params, params, determinism, trial)
 
@@ -51,7 +58,7 @@ class FinishedOrPruned(StopCondition):
         self.trial = trial
 
     def remaining_steps(self, trainer: 'Trainer') -> int:
-        max_update_steps = trainer.params.n_epochs * len(trainer.train_loader)
+        max_update_steps = trainer.params.n_epochs * len(trainer.labelled_train_loader)
         print(f"max_update_steps={max_update_steps}")
         return max_update_steps
 
@@ -78,9 +85,13 @@ class TqdmLoggingHandler(logging.Handler):
 
 
 @memory.cache(ignore=["trial"])
-def objective_run(dataset_params, training_params, trial):
-    trainer = Trainer(training_params, verbose=True)
-    labelled_train_loader, unlabelled_train_loader, val_loader = make_datasets(dataset_params, trainer.transform)
+def objective_run(dataset_params, training_params, determinism, trial):
+    trainer = Trainer(training_params, determinism, verbose=True)
+    labelled_train_loader, unlabelled_train_loader, val_loader = make_datasets(
+        dataset_params,
+        trainer.base_transform,
+        trainer.training_transform
+    )
     trainer.load_dataset(labelled_train_loader, unlabelled_train_loader, val_loader)
     result = trainer.train(stop_condition=FinishedOrPruned(trial))
     if len(result.epochs) < trainer.params.n_epochs:  # This means we stopped prematurely, before all epochs were finished
@@ -89,7 +100,8 @@ def objective_run(dataset_params, training_params, trial):
     return latest_val_acc
 
 
-def run_search(dataset_params: DatasetParams, training_params: TrainParams, search_params: SearchParams, determinism: Determinism, n_trials: int):
+def run_search(dataset_params: DatasetParams, training_params: TrainParams, search_params: SearchParams,
+               determinism: Determinism, n_trials: int):
     tqdm.write("Running study with dataset parameters:")
     tqdm.write(dataset_params.pprint())
     tqdm.write("and training parameters:")
@@ -116,7 +128,8 @@ def run_search(dataset_params: DatasetParams, training_params: TrainParams, sear
     handler.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
     optuna_logger.addHandler(handler)
 
-    study.optimize(lambda trial: objective(dataset_params, training_params, search_params, determinism, trial), n_trials=n_trials)
+    study.optimize(lambda trial: objective(dataset_params, training_params, search_params, determinism, trial),
+                   n_trials=n_trials)
 
     return {
         "trial_accuracies": [t.value for t in study.trials],
